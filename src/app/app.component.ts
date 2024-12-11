70daeeb4614340c4f64e56bc025058e01f701b25
Developer 6 <developer6@stableshield.com>
2024-12-11 05:11:39 +0100

    creating of component with CLI  and including importing that components for use in another component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatCardModule,MatButtonModule],
+  imports: [RouterOutlet, MatCardModule,MatButtonModule,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
