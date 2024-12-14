ab2cfadff3f76b8286a11b5780df1397eb08d0b8
Blessing-Paul <onublessingpaul@gmail.com>
2024-12-14 05:55:41 +0100

    pipes and using different types of pipes formatting pipes - date/currency and passing different parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { HomeComponent } from './home/home.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatCardModule,MatButtonModule,HomeComponent],
+  imports: [RouterOutlet, CommonModule, MatCardModule,MatButtonModule,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'standaloneApp';
   subTitle = 'angular 18 tutorial';
+  todayDate = new Date();
+  salary = 1000;
+  person = {"name": "Blessing"}
+  number = 2;
 }
