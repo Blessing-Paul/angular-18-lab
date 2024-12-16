6993fdec2252e511f33c93e34df9ef68b0ea0262
Blessing-Paul <onublessingpaul@gmail.com>
2024-12-16 16:19:22 +0100

    event binding and two way binding using ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { HomeComponent } from './home/home.component';
 import { CommonModule } from '@angular/common';
 import { ReversePipe } from './custom/reverse.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, MatCardModule,MatButtonModule,HomeComponent, ReversePipe],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    HomeComponent,
+    ReversePipe,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'standaloneApp';
   subTitle = 'angular 18 tutorial';
   todayDate = new Date();
   salary = 1000;
-  _obj = {"name": "Blessing"}
+  _obj = { name: 'Blessing' };
   number = 2;
   isDisabled = true;
+
+  changeTitle() {
+    this.title = 'Stand Alone Application in Angular 18';
+  }
+
+  updateTitle(event: any) {
+    this.title = event.target.value;
+  }
 }
