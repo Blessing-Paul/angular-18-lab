6858538f8af39e517db2b155606869d69b9efb12
Blessing-Paul <onublessingpaul@gmail.com>
2025-01-07 14:20:35 +0100

    Adding component/ routing for a path not found Creating child routes Getting parameters from navaigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,30 +3,47 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { SupportComponent } from './pages/support/support.component';
+import { CustomerComponent } from './pages/customer/customer.component';
+import { CustomerAddComponent } from './pages/customer-add/customer-add.component';
+import { FallbackComponent } from './fallback/fallback.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    },
-    {
-        path: 'about',
-        component: AboutComponent  
-    },
-    {
-        path: 'about/:submenu/:id',
-        component: AboutComponent  
-    },
-    {
-        path: 'contact',
-        component: ContactComponent
-    },
-    {
-        path: 'support',
-        component: SupportComponent
-    },
-    {
-        path: '**',
-        redirectTo: ''
-    }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'about/:submenu/:id',
+    component: AboutComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'support',
+    component: SupportComponent,
+  },
+  {
+    path: 'customer',
+    component: CustomerComponent,
+    children: [
+      {
+        path: 'add-customer',
+        component: CustomerAddComponent,
+      },
+      {
+        path: 'edit-customer/:id',
+        component: CustomerAddComponent
+      }
+    ],
+  },
+  {
+    path: '**',
+    component: FallbackComponent
+  },
 ];
