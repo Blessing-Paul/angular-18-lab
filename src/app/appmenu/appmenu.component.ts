a3b0968472b4fbca9de36f0aae061bb334f41db9
Blessing-Paul <onublessingpaul@gmail.com>
2025-03-24 12:59:22 +0100

    adding appmenu.html tag to app.html page adding logic for displaying navigation

--- src/app/appmenu/appmenu.component.ts
+++ src/app/appmenu/appmenu.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, DoCheck } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-appmenu',
-  imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './appmenu.component.html',
   styleUrl: './appmenu.component.css'
 })
-export class AppmenuComponent {
+export class AppmenuComponent implements DoCheck{
+  showMenu = false;
+
+  constructor (private router:Router){}
+
+  
+
+  ngDoCheck(): void {
+    let currentUrl = this.router.url
+    if(currentUrl === "/login"){
+      this.showMenu = false;
+    }else {
+      this.showMenu = true
+    }
+  }
+
 
 }
