9d541e39a058bf8fac497b84dbcdf63f5865c10e
Blessing-Paul <onublessingpaul@gmail.com>
2025-04-24 14:18:51 +0100

    added function to get list of products in products.ts file

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../service/product.service';
+import { Products } from '../../model/ProductModel';
 
 @Component({
   selector: 'app-product',
   imports: [],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrl: './product.component.css',
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  constructor(private service: ProductService) {}
 
+  productList: Products[] = [];
+
+  ngOnInit(): void {
+    this.LoadProduct();
+  }
+
+  LoadProduct() {
+    this.service.GetAll().subscribe((item) => {
+      this.productList = item;
+    });
+  }
 }
